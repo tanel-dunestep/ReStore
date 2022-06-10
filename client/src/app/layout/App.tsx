3bad90601479102a9836a9e8c06c61493a00f5a6
tanel-dunestep <tanel@dunestep.ee>
2022-06-10 23:58:19 +0300

    After Section 9

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@mui/material";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
 import createTheme from "@mui/material/styles/createTheme";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
@@ -15,28 +15,35 @@ import "react-toastify/dist/ReactToastify.css";
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
 import BasketPage from "../../features/basket/BasketPage";
-import agent from "../api/agent";
-import { getCookie } from "../util/util";
 import LoadingComponent from "./LoadingComponent";
 import CheckoutPage from "../../features/checkout/CheckoutPage";
 import { useAppDispatch } from "../store/configureStore";
-import { setBasket } from "../../features/basket/basketSlice";
+import { fetchBasketAsync } from "../../features/basket/basketSlice";
+import Login from "../../features/account/Login";
+import Register from "../../features/account/Register";
+import { fetchCurrentUser } from "../../features/account/accountSlice";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
 	//const { setBasket } = useStoreContext();
 	const dispatch = useAppDispatch();
 	const [loading, setLoading] = useState(true);
-	const [darkMode, setDarkMode] = useState(false);
+
+	const initApp = useCallback(async () => {
+		try {
+			await dispatch(fetchCurrentUser());
+			await dispatch(fetchBasketAsync());
+		} catch (error) {
+			console.log(error);
+		}
+	}, [dispatch]);
 
 	useEffect(() => {
-		const buyerId = getCookie("buyerId");
-		if (buyerId) {
-			agent.Basket.get()
-				.then((basket) => dispatch(setBasket(basket)))
-				.catch((error) => console.log(error))
-				.finally(() => setLoading(false));
-		} else setLoading(false);
-	}, [dispatch]);
+		initApp().then(() => setLoading(false));
+	}, [initApp]);
+
+	const [darkMode, setDarkMode] = useState(false);
+
 	function handleThemeChange() {
 		setDarkMode(!darkMode);
 	}
@@ -58,10 +65,12 @@ function App() {
 					<Route exact path="/catalog" component={Catalog}></Route>
 					<Route path="/catalog/:id" component={ProductDetails}></Route>
 					<Route path="/basket" component={BasketPage}></Route>
-					<Route path="/checkout" component={CheckoutPage}></Route>
 					<Route path="/about" component={AboutPage}></Route>
 					<Route path="/contact" component={ContactPage}></Route>
 					<Route path="/server-error" component={ServerError}></Route>
+					<PrivateRoute path="/checkout" component={CheckoutPage} />
+					<Route path="/login" component={Login}></Route>
+					<Route path="/register" component={Register}></Route>
 					<Route component={NotFound}></Route>
 				</Switch>
 			</Container>
